401485e623b27b2b42efcf28c1a3351a25d39caa
Mozy Lee <whyalwaysmeyy@gmail.com>
2020-11-04 11:17:32 +0900

    refactor : Finish app.js

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
-const screen = document.querySelector('.screen');
-const btnClear = document.querySelector('.btn-clear');
-const btnEqual = document.querySelector('.btn-equal');
-const btns = document.querySelectorAll('.btn');
+const screen = document.querySelector(".screen");
+const btnClear = document.querySelector(".btn-clear");
+const btnEqual = document.querySelector(".btn-equal");
+const btns = document.querySelectorAll(".btn");
+const regex = new RegExp(/[+\-\*/]/, "g");
 
-const returnBtnValue=(e)=>{
-    screen.value += e.target.dataset.num;
-    return e.target.dataset.num;
-}
-btns.forEach((btn)=>{
-    btn.addEventListener('click', returnBtnValue);
-});
-btnClear.addEventListener('click', ()=>{
-    console.log('clear')
-    screen.value = ''
-});
-btnEqual.addEventListener('click', ()=>{
+const isError = (btn) => {
+  const lastChar = screen.value.charAt(screen.value.length - 1);
 
+  if (
+    btn.classList.value.match("btn-yellow") &&
+    (!screen.value || lastChar.match(regex))
+  )
+    return true;
+  const amountOfOperator = screen.value.match(regex)
+    ? screen.value.match(regex).length
+    : 0;
+  const amountOfdot = screen.value.match(/\./)
+    ? screen.value.match(/\./).length
+    : 0;
+  console.log(screen.value, { amountOfOperator, amountOfdot });
+  if (
+    btn.dataset.num === "." &&
+    (lastChar === "." || amountOfdot > amountOfOperator)
+  )
+    return true;
+
+  return false;
+};
+const updateScreenValue = ({ target }) => {
+  if (isError(target)) return false;
+  if (!screen.value && target.dataset.num === ".") screen.value = "0";
+  screen.value += target.dataset.num;
+};
+btns.forEach((btn) => {
+  btn.addEventListener("click", updateScreenValue);
+});
+btnClear.addEventListener("click", () => {
+  screen.value = "";
+});
+btnEqual.addEventListener("click", () => {
+  if (!screen.value) {
+    screen.value = "Please Enter a Value";
+    return false;
+  }
+  screen.value = eval(screen.value);
 });
